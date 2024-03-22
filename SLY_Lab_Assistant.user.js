912488e5964735155bfd08a89f607d1e2742497a
HattoriHanzo19 <161739733+HattoriHanzo19@users.noreply.github.com>
2024-03-22 22:30:27 +0100

    Update SLY_Lab_Assistant.user.js

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -20,8 +20,8 @@
 
     let enableAssistant = false;
     let initComplete = false;
-    let rpcEndpoints = ['https://rpc.hellomoon.io/cfd5910f-fb7d-4489-9b32-f97193eceefd','https://solana-api.syndica.io/access-token/WPoEqWQ2auQQY1zHRNGJyRBkvfOLqw58FqYucdYtmy8q9Z84MBWwqtfVf8jKhcFh/rpc'];
-    const priorityFee = 1; // Priority Fee added to each transaction in Lamports. Set to 0 (zero) to disable priority fees. 1 Lamport = 0.000000001 SOL
+    let rpcEndpoints = ['https://solana-mainnet.g.alchemy.com/v2/2to-dTsLCcwiURbjTREFIeNC3DTnL9i2','https://rpc.hellomoon.io/64e1d4f8-7f66-4808-be22-cab7a9053e72'];
+    const priorityFee = 1000; // Priority Fee added to each transaction in Lamports. Set to 0 (zero) to disable priority fees. 1 Lamport = 0.000000001 SOL
 
     const connectionProxy = {
         get(target, key, receiver) {
